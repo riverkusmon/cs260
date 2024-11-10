77860de29038f87f2d7d2ad27b8cc0decbd53a7f
riverkusmon <andrewgardner@live.com>
2024-11-09 20:41:53 -0700

    created a navidation component in my layout file

---
+++ src/components/Layout.jsx
@@ -0,0 +1,12 @@
+export function Navigation() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/previous-grants">Previous Grants</a></li>
+                <li><a href="/new-grant">New Grant</a></li>
+                <li><a href="/sign-in">Sign In</a></li>
+            </ul>
+        </nav>
+    );
+}
